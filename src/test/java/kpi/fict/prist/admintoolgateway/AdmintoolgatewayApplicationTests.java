25770740eefae26f16382020a7774ab64ea8dbb4
Bogdan Zaranik <bogdan.zaranik@appliedmethods.biz>
2024-12-28 21:12:50 +0200

    initial

---
+++ src/test/java/kpi/fict/prist/admintoolgateway/AdmintoolgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kpi.fict.prist.admintoolgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmintoolgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
